6a0afb45179d6311a37cabb82f1d86529faf3d6b
DivyasreeGandikota <gandikota.d@northeastern.edu>
2022-12-10 20:31:30 -0500

    Adding Business models

---
+++ FINALproject/src/Business/Organization/NGOAdminOrganization.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import java.util.ArrayList;
+import Business.Role.AdminRole;
+import Business.Role.NGORole;
+import Business.Role.Role;
+import javax.management.relation.Role;
+
+/**
+ *
+ * @author bunny
+ */
+public class NGOAdminOrganization extends Organization{
+    
+    
+     public NGOAdminOrganization() {
+        super(Organization.Type.NgoFoodOrganization.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new NGORole());
+        return roles;
+    }
+    
+    
+    
+}
