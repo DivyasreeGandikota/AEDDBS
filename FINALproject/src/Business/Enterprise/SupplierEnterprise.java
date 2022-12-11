b0ff314d0a5322ff498de3bf8f86b641a6e6ed54
BhaktiDesaiB <desai.bhak@northeastern.edu>
2022-12-11 01:02:36 -0500

    adding enterprise

---
+++ FINALproject/src/Business/Enterprise/SupplierEnterprise.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author db
+ */
+public class SupplierEnterprise extends Enterprise{
+        public SupplierEnterprise(String name){
+        super(name,Enterprise.EnterpriseType.Supplier);
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
