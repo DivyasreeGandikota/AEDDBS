2ef301614078c8b69a8118608686843e3e7d8f8f
DivyasreeGandikota <gandikota.d@northeastern.edu>
2022-12-11 20:48:52 -0500

    adding business files

---
+++ FINALproject/src/Business/Organization/SupplierOrganization.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import Business.Role.Role;
+import Business.Role.SupplierRole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bunny
+ */
+public class SupplierOrganization {
+    
+    public SupplierOrganization() {
+        super(Organization.Type.SupplierFoodOrganization.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new SupplierRole());
+        return roles;
+    }
+    
+    
+}
